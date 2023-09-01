89b75bf8056a8c737043ac79d6f1bcd5ff76c745
David Gallegos <davidmartingallegos@gmail.com>
2023-09-01 09:44:33 -0300

    chore

--- scripts.js
+++ scripts.js
@@ -1,5 +1,9 @@
-const box = document.querySelector("#box1");
 const body = document.body;
+const boxes = document.getElementsByClassName("box");
+
+let currentIndex = 1;
+let currentBox = document.getElementById(`box${currentIndex}`);
+currentBox.classList.add("active");
 
 let transformationValues = {
   perspective: 1250,
@@ -14,8 +18,8 @@ let pointerMove = (e) => {
   var y = e.pageY / window.innerHeight - 0.5;
   transformationValues = {
     ...transformationValues,
-    rotateX: y * 100 + 60,
-    rotateZ: -x * 100 + 45,
+    rotateX: y * 30 + 60,
+    rotateZ: -x * 30 + 45,
   };
   applyTransform();
 };
@@ -31,14 +35,50 @@ let zoom = (event) => {
 };
 
 let applyTransform = () => {
-  box.style.transform = `
-    perspective(${transformationValues.perspective}px)
-    rotateX(${transformationValues.rotateX}deg)
-    rotateZ(${transformationValues.rotateZ}deg)
-    translateZ(${transformationValues.translateZ}px)
-    scale(${transformationValues.scale})
-  `;
+  Array.from(boxes).forEach((box) => {
+    box.style.transform = `
+      perspective(${transformationValues.perspective}px)
+      rotateX(${transformationValues.rotateX}deg)
+      rotateZ(${transformationValues.rotateZ}deg)
+      translateZ(${transformationValues.translateZ}px)
+      scale(${transformationValues.scale})
+    `;
+  });
 };
 
-//body.addEventListener("pointermove", pointerMove);
+body.addEventListener("pointermove", pointerMove);
 //body.onwheel = zoom;
+
+/** Toggler */
+const toggler = document.getElementById("toggle");
+
+let toggle = (event) => {
+  event.preventDefault();
+  currentBox.classList.toggle("active");
+};
+
+toggler.addEventListener("click", toggle);
+
+/** Next & prev */
+const prevButton = document.getElementById("prev");
+const nextButton = document.getElementById("next");
+
+let changeBox = (event, direction) => {
+  event.preventDefault();
+
+  const newIndex = direction === "prev" ? currentIndex - 1 : currentIndex + 1;
+  const newBox = document.getElementById(`box${newIndex}`);
+
+  if (newBox) {
+    currentBox.classList.remove("active");
+
+    setTimeout(() => {
+      newBox.classList.add("active");
+      currentIndex = newIndex;
+      currentBox = newBox;
+    }, 1000);
+  }
+};
+
+prevButton.addEventListener("click", (event) => changeBox(event, "prev"));
+nextButton.addEventListener("click", (event) => changeBox(event, "next"));
